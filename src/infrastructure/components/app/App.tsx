bf8998539c576399dca5e0cbd807afa120d20fd7
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-26 11:08:07 +0200

    Initial commit

---
+++ src/infrastructure/components/app/App.tsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+function App() {
+    return (
+        <div className="app">
+            {" "}
+            <h1>React App Working (Remove this h1 from App.tsx)</h1>{" "}
+        </div>
+    );
+}
+
+export default App;
